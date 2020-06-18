74d60f60ae84dd1a53cd69c76aeb8bd709438c15
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-18 17:10:03 +0200

    Restarted library for new roadmap

---
+++ source/libutterfly.d
@@ -0,0 +1,18 @@
+module source.libutterfly;
+
+import std.socket;
+
+public final class ButterflyClient
+{
+    this(string besterEndpoint, string[] credentials)
+    {
+        try
+        {
+            //Address  
+        }
+        catch(AddressException e)
+        {
+
+        }
+    }
+}
