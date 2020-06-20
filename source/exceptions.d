bf03c42bb84d51ad1ce4d2117f994eb99ec7b718
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-20 15:30:29 +0200

    WIP: Exception handling

--- source/exceptions.d
+++ source/exceptions.d
@@ -1,6 +1,25 @@
 module libutterfly.exceptions;
 
-public class ButterflyException
+/* TODO: Work in progress */
+public class ButterflyException : Exception
 {
-    
+
+    public string message;
+    public long statusCode;
+
+    this(string message, long statusCode)
+    {
+        super(message);
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+/* TODO: Work in progress */
+public final class MailSendException : ButterflyException
+{
+    this(long statusCode)
+    {
+        super("", statusCode);
+    }
 }
